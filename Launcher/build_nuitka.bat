166f138da346d6228413287a72ebeca3efbd34b3
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-12-11 18:42:55 +0530

    Delete Launcher directory

--- Launcher/build_nuitka.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-py -OO -m nuitka --onefile --standalone --follow-imports --include-plugin-files=modules\launch.py --include-plugin-files=modules\config.py main.py --output-dir="%TEMP%" -o "Launch.exe"
-timeout 5
