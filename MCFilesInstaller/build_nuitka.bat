8f4713ef30088f63b1aee6680e92cafe1ee75144
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-12-11 18:43:00 +0530

    Delete MCFilesInstaller directory

--- MCFilesInstaller/build_nuitka.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-py -OO -m nuitka --onefile --standalone --follow-imports --include-plugin-files=install.py main.py --output-dir="%TEMP%" -o "MCFilesInstaller.exe"
-timeout 5
--- MCFilesInstaller/main.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import install
-parser=argparse.ArgumentParser()
-parser.add_argument('-i', 
-                    nargs=1, 
-                    action="store")
-args=parser.parse_args()
-if args.i is not None:
-    install.install(args.i[0])
