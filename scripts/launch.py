200798a57afa48e58834b18c7ec34ce9c1693ec0
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-12-31 21:48:02 +0530

    Single Launch Variable + Log4j Fix.

--- scripts/launch.py
+++ scripts/launch.py
@@ -2,53 +2,51 @@
 import subprocess
 import os
 import config
-
-
 def Launch(Version):
+    MCVersion=""
+    if Version=="1.7":
+        MCVersion="1.7.10" 
+    elif Version=="1.8":
+        MCVersion="1.8.9"
+    elif Version=="1.12":
+        MCVersion="1.12.2" 
+    elif Version=="1.16":
+        MCVersion="1.16.5"
+    elif Version=="1.17":
+        MCVersion="1.17.1"
+    elif Version=="1.18":
+        MCVersion="1.18.1"
     # Read Values from Options.ini
     Config=config.ConfigRead(Version)
-    Natives=os.getcwd()+"\\.lunarclient_files\\offline\\"+Version+"\\natives"
+    VersionDir=os.getcwd()+"/.portaluna/game/versions/"+MCVersion
     # Set the Asset Index
     if Version=="1.7":
         AssetIndex="1.7.10"
     else:
         AssetIndex=Version		
     # Launch Variable  
-    Launch_1=[Config[0],
-	"--add-modules",
+    Launch=[Config[0],"--add-modules",
 	"jdk.naming.dns",
 	"--add-exports",
 	"jdk.naming.dns/com.sun.jndi.dns=java.naming",
-	"-Djna.boot.library.path="+Natives,
-	"--add-opens",
-	"java.base/java.io=ALL-UNNAMED"]
-    Launch_2=["-Djava.library.path="+Natives,
-        "-XX:+DisableAttachMechanism",
-	"-cp",
-	".lunarclient_files\\offline\\"+Version+"\\lunar-assets-prod-1-optifine.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\lunar-assets-prod-2-optifine.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\lunar-assets-prod-3-optifine.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\lunar-libs.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\lunar-prod-optifine.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\OptiFine.jar;"
-	+".lunarclient_files\\offline\\"+Version+"\\vpatcher-prod.jar",
+	"-Djna.boot.library.path="+VersionDir+"/natives",
+	"-Dlog4j2.formatMsgNoLookups=true",
+	"--add-opens","java.base/java.io=ALL-UNNAMED"]+Config[1]+["-Djava.library.path="+VersionDir+"/natives",
+	"-XX:+DisableAttachMechanism","-cp",
+	VersionDir+"/lunar-assets-prod-1-optifine.jar;"
+	+VersionDir+"/lunar-assets-prod-2-optifine.jar;"
+	+VersionDir+"/lunar-assets-prod-3-optifine.jar;"
+	+VersionDir+"/lunar-libs.jar;"
+	+VersionDir+"/lunar-prod-optifine.jar;"
+	+VersionDir+"/OptiFine.jar;"
+	+VersionDir+"/vpatcher-prod.jar",
 	"com.moonsworth.lunar.patcher.LunarMain",
-	"--version",
-	str(Version),
-	"--accessToken",
-	"0", 
-	"--assetIndex",
-	str(AssetIndex),
-	"--userProperties",
-	"{}",
-	"--gameDir",
-	".minecraft\\"+Version,
-	"--width",
-	"854",
-	"--height",
-	"480",
-	"--assetsDir",
-	".minecraft_files\\assets"]
-
-    Launch=Launch_1+Config[1]+Launch_2
+	"--version",str(Version),
+	"--accessToken","0", 
+	"--assetIndex",str(AssetIndex),
+	"--userProperties","{}",
+	"--gameDir", ".minecraft\\"+Version,
+	"--width","854",
+	"--height","480",
+	"--assetsDir",".portaluna/game/assets"]
     subprocess.Popen(Launch)
