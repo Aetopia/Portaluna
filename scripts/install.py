1bcaacbff802ae28541e093c727cf7bddc32c134
Aetopia <41850963+Aetopia@users.noreply.github.com>
2021-12-31 21:47:24 +0530

    Revamped Installation Script.

--- scripts/install.py
+++ scripts/install.py
@@ -1,77 +1,104 @@
-import minecraft_launcher_lib as MCLib
-import subprocess
+import requests
+import os
+import urllib.request 
+import shutil
+import minecraft_launcher_lib
+import ctypes
+
+Dirs=(".portaluna/game",".portaluna/licenses",".portaluna/jre")
 max_value = [0]
 
-def Install(Version):
-    InstallLC(Version)
-    InstallMC(Version)
+def AssetIndex(Version):
+    MCVersion=""
 
-def InstallMC(Version):
-    if Version == "1.7.10" or Version == "1.8.9" or Version == "1.12.2" or Version == "1.16.5" or Version == "1.17.1" or Version == "1.18.1":
-        pass
-    else:
-        while True:
-            print("Specified Version isn't supported by LC.")
-            pause=input()
-            exit()
+    if Version=="1.7":
+        MCVersion="1.7.10" 
+    elif Version=="1.8":
+        MCVersion="1.8.9"
 
-    max_value = [0]
+    elif Version=="1.12":
+        MCVersion="1.12.2" 
 
-    callback = {
-        "setStatus": lambda text: print(text+": "),
-        "setProgress": lambda value: printProgressBar(value, max_value[0]),
-        "setMax": lambda value: maximum(max_value, value)
-    } 
-    print ("Installing", "("+Version+")"+"...")       
-    MCLib.install.install_minecraft_version(Version, ".minecraft_files", callback=callback)
+    elif Version=="1.16":
+        MCVersion="1.16.5"
+
+    elif Version=="1.17":
+        MCVersion="1.17.1"
+
+    elif Version=="1.18":
+        MCVersion="1.18.1"
+
+    return MCVersion 
 
 def InstallLC(Version):
-    LCVersion=""
-    if Version=="1.7.10":
-        LCVersion="1.7"
-        
-    elif Version=="1.8.9":
-        LCVersion="1.8"
-        
-    elif Version=="1.12.2":
-        LCVersion="1.12"
-        
-    elif Version=="1.16.5":
-        LCVersion="1.8"
-        
-    elif Version=="1.17.1":
-        LCVersion="1.17"
+    MCVersion=AssetIndex(Version)
 
-    elif Version=="1.18.1":
-        LCVersion="1.18"
-        
-    subprocess.run(["powershell",
-                    "iex",
-                    '"&',
-                    '{$(irm https://raw.githubusercontent.com/Aetopia/Lunar-Client-Mini/main/LCFilesInstaller.ps1)}',
-                    LCVersion+'"'])    
-        
-    
+    print("Installing Lunar Client","("+Version+")"+"...")
+    for Index in range(len(Dirs)):    
+        if os.path.exists(Dirs[Index]) is False:
+            os.makedirs(Dirs[Index])
 
-def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    if os.path.exists(Dirs[0]+"/versions/"+MCVersion) is False:
+        os.makedirs(Dirs[0]+"/versions/"+MCVersion)
+        VersionDir=Dirs[0]+"/versions/"+MCVersion
+    else:    
+        VersionDir=Dirs[0]+"/versions/"+MCVersion
+
+    if os.path.exists(VersionDir+"/"+"natives") is False:
+        os.mkdir(VersionDir+"/"+"natives")
+
+    Params={"hwid":"0","hwid_private":"0","os":"win32","arch":"x64","launcher_version":"0","version":Version,"branch":"master","launch_type":"0","classifier":"0"} 
+    Response=requests.post('https://api.lunarclientprod.com/launcher/launch',json=Params)
+    Response=Response.json()
+    # Artifacts
+    for Index in range(len(Response.get("launchTypeData").get("artifacts"))):
+        Artifact=Response.get("launchTypeData").get("artifacts")[Index].get("url")
+        ArtifactName=Response.get("launchTypeData").get("artifacts")[Index].get("name")
+        print("Download", ArtifactName+"...") 
+        try:
+            urllib.request.urlretrieve(Artifact, VersionDir+"/"+ArtifactName)
+        except:    
+            break 
+    Natives=VersionDir+"/"+Response.get("launchTypeData").get("artifacts")[6].get("name")      
+    shutil.unpack_archive(Natives,VersionDir+"/"+"natives", "zip",)
+
+    #Licenses
+    for Index in range(len(Response.get("licenses"))):
+        License=Response.get("licenses")[Index].get("url")    
+        LicenseName=Response.get("licenses")[Index].get("file") 
+        print("Download", LicenseName+"...") 
+        try:
+            urllib.request.urlretrieve(License, VersionDir+"/"+ArtifactName)
+        except:    
+            break  
+    # Java Runtime Environment   
+    if os.path.exists(Dirs[2]+"/"+Response.get("jre").get("executablePathInArchive")[0]) is False:
+        print("Download Lunar Client Java Runtime...")    
+        urllib.request.urlretrieve(Response.get("jre").get("download").get("url"), Dirs[2]+"/"+Response.get("jre").get("executablePathInArchive")[0]+"."+Response.get("jre").get("download").get("extension"))
+        print("Extract Lunar Client Java Runtime...")
+        shutil.unpack_archive(Dirs[2]+"/"+Response.get("jre").get("executablePathInArchive")[0]+"."+Response.get("jre").get("download").get("extension"), Dirs[2], "zip")
+        os.remove(Dirs[2]+"/"+Response.get("jre").get("executablePathInArchive")[0]+"."+Response.get("jre").get("download").get("extension"))
+
+def InstallMC(Version):
+    Version=AssetIndex(Version)
+    callback = {"setStatus": lambda text: print(text+"..."),"setProgress": lambda value: printProgressBar(value, max_value[0]),"setMax": lambda value: maximum(max_value, value)} 
+    print ("Installing", "("+Version+")"+"...")       
+    minecraft_launcher_lib.install.install_minecraft_version(Version, Dirs[0], callback=callback)
+
+def Install(Version):
+    ctypes.windll.kernel32.SetConsoleTitleW("Installing Minecraft ("+AssetIndex(Version)+")...")
+    InstallMC(Version)
+    print("")
+    ctypes.windll.kernel32.SetConsoleTitleW("Installing Lunar Client ("+Version+")...")
+    InstallLC(Version)
+    print("Finished!")
+
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=0, fill='█', printEnd="\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(0 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end=printEnd)
-    # Print New Line on Complete
     if iteration == total:
         print()
+
 def maximum(max_value, value):
-    max_value[0] = value         
+    max_value[0] = value              
